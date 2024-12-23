6927de42b7741d8a39aadfd213733f9a53f15ea8
Urbigmumz <140040348+Urbigmumz-dev@users.noreply.github.com>
2024-12-23 19:38:05 +0000

    fix RAM usages

--- start.bat
+++ start.bat
@@ -6,10 +6,10 @@ start "BungeeCord" java -Xms256M -Xmx512M -jar ./BungeeCord.jar nogui
 
 echo Starting Void Server...
 cd void_lobby
-start "Void Lobby" java -Xms256M -Xmx512M -jar ./server.jar nogui
+start "Void Lobby" java -Xms512M -Xmx1024M -jar ./server.jar nogui
 
 echo Starting Main Server...
 cd ../lobby
-start "Lobby" java -Xms512M -Xmx2G -jar ./server.jar nogui
+start "Lobby" java -Xms512M -Xmx6144M -jar ./server.jar nogui
 
 echo All servers started. Press any key to exit this window.
